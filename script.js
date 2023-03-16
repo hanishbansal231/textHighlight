2f3af4f8ea0e39c82865966a54ecd4430b3274ee
hanishbansal231 <108611960+hanishbansal231@users.noreply.github.com>
2023-03-16 18:09:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,15 @@
+const btn = document.querySelector('.btn');
 
-let btn = document.querySelector('.btn');
-
-btn.addEventListener('click', () => {
-
-  let pra = document.querySelector('p').textContent;
-
-  console.log(pra)
-  let text = pra.split(" ");
-  // console.log(text)
-  for (let i = 0; i < text.length; i++) {
-    if (text[i].length > 8) {
-      //   span.textContent = text[i];
-      //   span.setAttribute('class', 'highlight');
-      //  text[i] = span.outerHTML;
-      // console.log(text[i]);
-      const span = document.createElement('span');
+btn.addEventListener('click', () =>{
+  let pra = document.querySelector('p');
+  let text = pra.textContent.split(" ");
+  let span = document.createElement('span');
+  for(let i = 0;i < text.length;i++){
+    if(text[i].length > 8){
       span.textContent = text[i];
-      // span.className = 'highlight';
-      span.setAttribute('class','highlight');
-      console.log(span.outerHTML);
+      span.classList.add('highlight');
       text[i] = span.outerHTML;
     }
   }
-  console.log(text)
-  // pra.innerHTML = text.join(' ')
-  pra.textContent = text.join(" ");
-
-
-
+  pra.innerHTML = text.join(" ");
 });
